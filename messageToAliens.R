dfd0ede66a9e9f8e2491da48115d0d73c073d5be
Mackenzie <apple@Mac.wv.cc.cmu.edu>
2017-12-10 17:08:09 -0500

    message

--- messageToAliens.R
+++ messageToAliens.R
@@ -1,4 +1,4 @@
-cat("Hi there! Welcome to Earth. My name is Chi Fang. I would like to introduce my sweet home- 
+cat("Hi there! Welcome to Earth. My name is Chi Fang. I would like to introduce my sweet home: 
 Earth to you. As a space traveler, the distinguishing Earth features are the blue waters, brown and 
     green land masses and white clouds set against a black background.as far as I know, Earth is the 
     3rd planet from the Sun at a distance of about 150 million kilometers (93.2 million miles).
