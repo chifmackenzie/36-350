38efd01b6956556cd7eb02661476f02ab5c1cb7e
Mackenzie <apple@Mac.wv.cc.cmu.edu>
2017-12-10 17:06:34 -0500

    message

---
+++ messageToAliens.R
@@ -0,0 +1,6 @@
+cat("Hi there! Welcome to Earth. My name is Chi Fang. I would like to introduce my sweet home- 
+Earth to you. As a space traveler, the distinguishing Earth features are the blue waters, brown and 
+    green land masses and white clouds set against a black background.as far as I know, Earth is the 
+    3rd planet from the Sun at a distance of about 150 million kilometers (93.2 million miles).
+    I thought Earth is the only planet in the solar system known to harbor life, but now we see you!
+    Great! Enjoy your stay here!")
